e5c41cf720168ed00c2237dcc568faa260703d29
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2023-08-09 21:40:58 -0700

    Poker Game
    Poker Game

--- Poker_Game/Poker_game.py
+++ Poker_Game/Poker_game.py
@@ -1,1 +1,45 @@
+# POKER GAME
+import random
 
+class Card:
+    
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+        
+    def __repr__(self):
+        return f"{self.suit} --> {self.value}"
+    
+    
+class Deck:
+    def __init__(self):
+        
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"]
+        
+        self.cards = [Card(suit, value) for suit in suits for value in values]
+        
+    def __repr__(self):
+        return "Cards left " + str(len(self.cards))
+    
+    
+    def shuffle(self):
+        if len(self.cards) < 52:
+            print("Sorry ! all the cards should be shuffled")
+            
+        else:
+            random.shuffle(self.cards)
+        return self.cards
+            
+    def deal(self):
+        if len(self.cards) == 0:
+            print("All the cards are dealt")
+        else:
+            return self.cards.pop()
+        
+        
+deck = Deck()
+deck.shuffle()
+print(deck.deal())
+print(deck.deal())
+print(deck)
